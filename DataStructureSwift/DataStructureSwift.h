d03ffac3336ab5d750356baa065e8b062267be94
wttch（王冲） <wttch@wttch.com>
2022-08-03 11:23:37 +0800

    Initial Commit

---
+++ DataStructureSwift/DataStructureSwift.h
@@ -0,0 +1,18 @@
+//
+//  DataStructureSwift.h
+//  DataStructureSwift
+//
+//  Created by Wttch on 2022/8/3.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DataStructureSwift.
+FOUNDATION_EXPORT double DataStructureSwiftVersionNumber;
+
+//! Project version string for DataStructureSwift.
+FOUNDATION_EXPORT const unsigned char DataStructureSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataStructureSwift/PublicHeader.h>
+
+
